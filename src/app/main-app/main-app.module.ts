53c1b59f34e919b5414924bbf2b370df7d0bcef7
fj994 <filip.jakovcevic1@gmail.com>
2020-04-08 00:45:41 +0200

    profile implementation

---
+++ src/app/main-app/main-app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainAppComponent } from './main-app.component';
+
+
+
+@NgModule({
+  declarations: [MainAppComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainAppModule { }
